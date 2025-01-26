d3604c83f6e0ed3ad7147a250cd83b026eb90db5
Josh <imperiojoshua@gmail.com>
2025-01-25 20:34:21 -0700

    Import new assets

--- InputAndAnimationGame.cs
+++ InputAndAnimationGame.cs
@@ -13,9 +13,9 @@ public class InputAndAnimationGame : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
-    private Texture2D _background, _bush;
-    private CelAnimationSequence _sequence01, _sequence02;
-    private CelAnimationPlayer _animation01, _animation02;
+    private Texture2D _background;
+    private CelAnimationSequence _flagSequence, _playerSequenceIdle, _playerSequenceWalk;
+    private CelAnimationPlayer _flagAnimation, _playerAnimationIdle, _playerAnimationWalk;
 
     public InputAndAnimationGame()
     {
@@ -36,7 +36,11 @@ public class InputAndAnimationGame : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-        _background = Content.Load<Texture2D>("PlaceholderBG"); // TODO: Replace placeholder
+
+        Texture2D playerSpritesheetIdle = Content.Load<Texture2D>("CharacterIdle");
+        Texture2D playerSpritesheetWalk = Content.Load<Texture2D>("CharacterWalk");
+
+        _background = Content.Load<Texture2D>("SnowBG"); 
     }
 
     protected override void Update(GameTime gameTime)
@@ -55,7 +59,7 @@ public class InputAndAnimationGame : Game
 
         _spriteBatch = new SpriteBatch(GraphicsDevice);
         _spriteBatch.Begin();
-
+        
         _spriteBatch.Draw(_background, Vector2.Zero, Color.White);
 
         _spriteBatch.End();
--- obj/Debug/net8.0/assignment01_animation_and_inputs.AssemblyInfo.cs
+++ obj/Debug/net8.0/assignment01_animation_and_inputs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("assignment01_animation_and_inputs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b024d12922486d466ce433f3ab2fd1060538e8f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad059bc8090a0ff8057a42e3813f72652fbcaedd")]
 [assembly: System.Reflection.AssemblyProductAttribute("assignment01_animation_and_inputs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("assignment01_animation_and_inputs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
